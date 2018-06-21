505fccd490842c91234d2ab8c863b63fa8b0c8ab
Jamie <jamieyu96@gmail.com>
2018-06-21 12:49:36 -0400

    added more ideas

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let ideas = [
 "Take a power nap",
 "Get rid of 3 things you never use",
 "Write down 3 things you're grateful for",
-"Don't use Facebook for the rest of the day",
 "Stop watching/reading the news for a week",
 "Write a journal entry",
 "Meditate",
@@ -19,10 +18,42 @@ let ideas = [
 "Power down your laptop, phone, and TV",
 "Write a letter to yourself",
 "Go on a run",
-"Start a personal project"
+"Start a personal project",
+"Go to your local art store, buy a watercolor kit, and paint",
+"Make avocado toast",
+"Bake sweet potato fries",
+"Organize your bedroom",
+"Find an event to attend this week",
+"Sing at the top of your lungs",
+"Join the Dogspotting group on Facebook",
+"Make a DIY face mask",
+"Learn how to code and build a website",
+"Bake banana bread",
+"Tell someone you love them",
+"Listen to Leon Bridges",
+"Make a new playlist on Spotify",
+"Delete your Snapchat/Instagram/Facebook for the day",
+"Sit outside",
+"Read a book",
+"Find a new recipe online and prepare it for dinner tonight",
+"Make a smoothie from scratch",
+"Buy a potted plant and name it",
+"Learn a new language",
+"Rearrange the furniture in your room",
+"Smile at the next person you make eye contact with"
 ];
 
+let usedNums = [];
+
 function generateIdea() {
-    let randInt = Math.floor(Math.random() * ideas.length);
-    document.getElementById('idea-container').innerHTML = ideas[randInt];
+    if (usedNums.length == ideas.length) {
+        document.getElementById('idea-container').innerHTML = "We are out of ideas... Send us an idea at jamieyu96@gmail.com!";
+    } else {
+        let randInt = 0;
+        while (usedNums.includes(randInt)) {
+            randInt = Math.floor(Math.random() * ideas.length);
+        }
+        usedNums.push(randInt);
+        document.getElementById('idea-container').innerHTML = ideas[randInt];
+    }
 }
